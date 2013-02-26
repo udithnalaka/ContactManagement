c9b62ca4b32e48e19247babfe3a7bc83f16d4b6b
udithnalaka <udithnalaka@yahoo.com.au>
2013-02-26 13:28:38 +1100

    add Around advice

--- src/main/java/com/udith/mycontacts/aspect/ContactLogger.java
+++ src/main/java/com/udith/mycontacts/aspect/ContactLogger.java
@@ -1,8 +1,10 @@
 package com.udith.mycontacts.aspect;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -32,20 +34,20 @@ public class ContactLogger {
   }
 
 
-//    @Around("execution(* com.udith.mycontacts.bo.ContactBO.saveContact(..))")
-//    public void logSaveContact(ProceedingJoinPoint jointpoint){
-//
-//        try {
-//          System.out.println("Logging:going to call saveContact() method in ContactBOImpl class...");
-//          long startTime = System.currentTimeMillis();
-//          jointpoint.proceed();
-//          long endTime = System.currentTimeMillis();
-//          System.out.println("Logging: call ended to  saveContact() method in ContactBOImpl class... ");
-//          System.out.println("Logging: saveContact() method in ContactBOImpl class took " + (endTime - startTime) + " milliseconds");
-//
-//        } catch (Throwable ex) {
-//           System.out.println("Logging: exception occured from  saveContact() method in ContactBOImpl class... ");
-//        }
-//    }
+    @Around("execution(* com.udith.mycontacts.bo.ContactBO.saveContact(..))")
+    public void logSaveContact(ProceedingJoinPoint jointpoint){
+
+        try {
+          System.out.println("Logging:going to call saveContact() method in ContactBOImpl class...");
+          long startTime = System.currentTimeMillis();
+          jointpoint.proceed();
+          long endTime = System.currentTimeMillis();
+          System.out.println("Logging: call ended to  saveContact() method in ContactBOImpl class... ");
+          System.out.println("Logging: saveContact() method in ContactBOImpl class took " + (endTime - startTime) + " milliseconds");
+
+        } catch (Throwable ex) {
+           System.out.println("Logging: exception occured from  saveContact() method in ContactBOImpl class... ");
+        }
+    }
 
 }
